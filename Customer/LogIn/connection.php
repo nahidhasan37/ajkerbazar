887e83c08f71c8b903c96ac905271d64d9724d1c
Md.Nahid Hasan <57107917+nahidhasan37@users.noreply.github.com>
2020-04-06 00:42:35 +0600

    Customized data

---
+++ Customer/LogIn/connection.php
@@ -0,0 +1,24 @@
+<?php
+require('db.php');
+session_start();
+if (isset($_POST['username'])){
+	$username = stripslashes($_REQUEST['username']);
+	$username = mysqli_real_escape_string($con,$username);
+	$password = stripslashes($_REQUEST['password']);
+	$password = mysqli_real_escape_string($con,$password);
+	
+$query="SELECT * FROM `login` WHERE username = '$username' and Password = '$password'";
+	$result = mysqli_query($con,$query) or die(mysql_error());
+	$rows = mysqli_num_rows($result);
+        if($rows==1){
+	    $_SESSION['Username'] = $username;
+	    header("Location: index.php");
+         }
+         else{
+	echo "<div class='form'>
+<h3>Username/password is incorrect.</h3>
+<br/>Click here to <a href='login.php'>Login</a></div>";
+	}
+	}else{
+	}
+?>
